40f44d9be843fe84bbe7713ab302e5d1d5acef4c
augustomentz <augusto.mentz@operacao.rcadigital.com.br>
2020-08-17 19:50:31 -0300

    Test CI

--- src/album.js
+++ src/album.js
@@ -3,15 +3,12 @@ import API_URL from './config';
 
 export const getAlbum = (id) => {
   fetch(`${API_URL}/albums/${id}`);
-  // .then(toJSON);
 };
 
 export const getAlbums = (ids) => {
   fetch(`${API_URL}/albums/?ids=${ids}`);
-  // .then(toJSON);
 };
 
 export const getAlbumTracks = (id) => {
   fetch(`${API_URL}/albums/${id}/tracks`);
-  // .then(toJSON);
 };
